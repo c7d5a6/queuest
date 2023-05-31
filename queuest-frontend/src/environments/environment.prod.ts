3fbf011268d41eac481ffe1cada74530ca067a15
Mikita Kukavenka <godinfrog@gmail.com>
2023-05-31 17:06:02 +0300

    gh-pages pre-release

--- queuest-frontend/src/environments/environment.prod.ts
+++ queuest-frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { ApplicationEnvironment } from './application-environment';
 export const environment: ApplicationEnvironment = {
   production: true,
   application: {
-    apiUrl: 'https://api.queuest.owlbeardm.com',
+    apiUrl: 'http://foundry.owlbeardm.com:3002',
   },
 };
