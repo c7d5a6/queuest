79cbf730fa28c6c1f1f5c6913d838b1f761d70bc
Mikita Kukavenka <nkukovenko@by.byelex.com>
2023-08-31 19:50:57 +0300

    Fixed

--- queuest-backend/src/persistence/orm.config.ts
+++ queuest-backend/src/persistence/orm.config.ts
@@ -1,6 +1,9 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
+
     type: 'postgres',
     host: process.env.DATABASE_HOST || 'localhost',
     port: parseInt(process.env.DATABASE_PORT || '5432', 10),
@@ -8,8 +11,13 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     password:  process.env.DATABASE_PASSWORD || 'queuest',
     database: 'queuest',
     entities: [__dirname + '/entities/*.entity{.ts,.js}'],
-    synchronize: false
-    // ssl:true
+    synchronize: false,
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
 };
 
 module.exports = typeOrmConfig;
