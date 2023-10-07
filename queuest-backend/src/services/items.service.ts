0466d68e5a52f69aec2037b9261c62225a5c58b4
c7d5a6 <godinfrog@gmail.com>
2023-10-08 01:09:38 +0300

    Auto stash before merge of "main" and "origin/main"

--- queuest-backend/src/services/items.service.ts
+++ queuest-backend/src/services/items.service.ts
@@ -107,9 +107,8 @@ export class ItemsService {
         };
     }
 
-    async setGraphEdges(items: CollectionItemEntity[], graph: Graph): Promise<void> {
-        const edge: Edges = await this.itemsRelationService.getRelationsMaps(items);
-        this.setGraphEdgesByEdges(items, edge, graph);
+    private isItemCalibrated(item: CollectionItemEntity, index: number, array: CollectionItemEntity[]):boolean {
+       return true;
     }
 
     async getBestPair(userUid: string, id: number, exclude?: number[]) {
@@ -174,8 +173,10 @@ export class ItemsService {
             collection: {id: collection.id},
         });
         const graph: Graph = new Graph(items.length);
-        await this.setGraphEdges(items, graph);
-        return this.getItemEntitySortedByGraph(graph, items);
+        const edge: Edges = await this.itemsRelationService.getRelationsMaps(items);
+        this.setGraphEdgesByEdges(items, edge, graph);
+        const sorted = this.getItemEntitySortedByGraph(graph, items);
+        this.isItemCalibrated(item,index,sorted,edge)
     }
 
     private getItemEntitySortedByGraph(graph: Graph, items: CollectionItemEntity[]) {
