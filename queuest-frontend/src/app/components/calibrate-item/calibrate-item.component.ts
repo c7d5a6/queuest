bb296176d0939bf83ce9ad18efd46a76140cb6ad
c7d5a6 <godinfrog@gmail.com>
2023-10-29 22:01:29 +0300

    added buttons calibration

--- queuest-frontend/src/app/components/calibrate-item/calibrate-item.component.ts
+++ queuest-frontend/src/app/components/calibrate-item/calibrate-item.component.ts
@@ -5,6 +5,7 @@ import {ItemsService} from '../../api/services/items.service';
 import {DialogRef} from "@ngneat/dialog";
 import {Data} from "@angular/router";
 import {ItemPair} from "../../api/models/item-pair";
+import {ItemsRelationService} from "../../api/services/items-relation.service";
 
 @Component({
   selector: 'app-calibrate-item',
@@ -21,12 +22,13 @@ export class CalibrateItemComponent implements OnInit {
   constructor(
     private ref: DialogRef<Data>,
     private itemsService: ItemsService,
+    private itemsRelationService: ItemsRelationService
   ) {
     console.log(ref.data);
-    if(ref.data['title']){
+    if (ref.data['title']) {
       this.title = ref.data['title']
     }
-    if(ref.data['itemId']){
+    if (ref.data['itemId']) {
       this.itemId = ref.data['itemId']
     }
   }
@@ -37,7 +39,7 @@ export class CalibrateItemComponent implements OnInit {
 
   itemPressed(id: number) {
     if (id === this.items.length - 1) {
-        this.getNextBestPair();
+      this.getNextBestPair();
     }
   }
 
@@ -46,19 +48,19 @@ export class CalibrateItemComponent implements OnInit {
     ids.push(this.itemId!);
     this.items.forEach((item) => ids.push(item.item2.id!));
     this.itemsService
-        .itemsControllerGetBestPair({
-            id: this.itemId!,
-            exclude: ids,
-        })
-        .subscribe((pair) => {
-            if (!!pair) {
-                this.items.push(pair);
-              this.calibrated = false;
-            } else {
-                this.calibrated = true;
-            }
-            console.log(pair,this.items.length)
-        });
+      .itemsControllerGetBestPair({
+        id: this.itemId!,
+        exclude: ids,
+      })
+      .subscribe((pair) => {
+        if (!!pair) {
+          this.items.push(pair);
+          this.calibrated = false;
+        } else {
+          this.calibrated = true;
+        }
+        console.log(pair, this.items.length)
+      });
   }
 
   ngOnInit(): void {
@@ -67,5 +69,39 @@ export class CalibrateItemComponent implements OnInit {
 
   close() {
     this.ref.close();
+  }
+
+  addRelation(pair: ItemPair, i: number, from: number | undefined, to: number | undefined) {
+    if (from !== undefined && to !== undefined) {
+      console.log('adding relation');
+      const relation = {
+        from: from,
+        to: to,
+      };
+      this.itemsRelationService
+        .itemRelationsControllerAddItem({
+          fromId: from,
+          toId: to
+        })
+        .subscribe(() => {
+          pair.relation = relation;
+          this.itemPressed(i);
+        });
+    }
+  }
+
+  deleteRelation(pair: ItemPair, i: number) {
+    console.log('deleting relation');
+    if (pair.relation) {
+      this.itemsRelationService
+        .itemRelationsControllerGetItems({
+          itemAId: pair.relation.from,
+          itemBId: pair.relation.to
+        })
+        .subscribe(() => {
+          pair.relation = undefined;
+          this.itemPressed(i);
+        });
+    }
   }
 }
