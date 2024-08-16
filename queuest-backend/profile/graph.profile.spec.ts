2ce45a1c385c6f1011537a03ac6519527da30c50
c7d5a6 <godinfrog@gmail.com>
2024-08-16 03:34:49 +0300

    profile

--- queuest-backend/profile/graph.profile.spec.ts
+++ queuest-backend/profile/graph.profile.spec.ts
@@ -3,30 +3,25 @@ import { GraphService } from '../src/services/graph.service';
 import { Graph } from '../src/models/graph';
 import { FunctionUtils } from '../src/utils/function.utils';
 
-describe('GraphService', () => {
-    let service: GraphService;
-    let graph: Graph;
-    const size = 500;
+const MAX_SIZE = 1_000_000_00;
 
-    beforeAll(() => {
-        graph = new Graph(size);
-        const d0 = new Date();
-        for (let i = 0; i < size; i++) {
-            for (let j = 0; j < size; j++) {
-                if (i != j) {
-                    // const cbr = FunctionUtils.cyrb53a(i, j);
-                    const cbr = (i * 3 + j);
-                    if (cbr % 11 === 0) {
-                        const cbrRev = FunctionUtils.cyrb53a(j, i);
-                        if ((j < i && cbrRev % 2 !== 0) || i < j)
-                            graph.addEdge(i, j);
-                    }
+function createGraph(n: number, del: number): Graph {
+    const graph = new Graph(n);
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i != j) {
+                const cbr = FunctionUtils.cyrb53a(i, j);
+                if (cbr % del === 0) {
+                    graph.addEdge(i, j);
                 }
             }
         }
-        const d1 = new Date();
-        console.log(`Initial Time Cost: ${d1.getTime() - d0.getTime()} ms`);
-    });
+    }
+    return graph;
+}
+
+describe('GraphService', () => {
+    let service: GraphService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -35,33 +30,42 @@ describe('GraphService', () => {
         service = module.get<GraphService>(GraphService);
     });
 
-    it('detect cyclic graph', () => {
-        expect(service.isGraphCylic(graph)).toBeDefined();
-    });
-
+    // it('detect cyclic graph', () => {
+    //     expect(service.isGraphCylic(graph)).toBeDefined();
+    // });
+    //
     it('detect is', () => {
-        var t = Date.now();
-        for (var i = 0; i < 1000; i++) {
-            expect(service.isGraphCylic(graph).length > 0).toBeTruthy();
+        for (var n = 1; n < 2000; n = n * 2) {
+            var times = MAX_SIZE / (n * n * n * n);
+            var t = 0;
+            var create = Date.now();
+            for (var del = 2; del < n; del++) {
+                const graph = createGraph(n, del);
+                var date = Date.now();
+                for (var i = 0; i < Math.max(1000, times); i++) {
+                    expect(service.isGraphCylic(graph).length >= 0).toBeTruthy();
+                }
+                t += Date.now() - date;
+            }
+            console.log(`Time for ${n} size created ${Date.now() - create - t} times ${Math.max(1000, times)} in ms: ${t}`);
         }
-        console.log("Time in ms:", (Date.now() - t));
     });
 
-    xit('detect cyclic graph', () => {
-        const initialArcs = graph.adj.flat();
-        const arcSet = service.feedbackArkSet(graph);
-        const arcs = arcSet.adj.flat();
-        console.log(
-            'ARC LENGTH',
-            initialArcs.length,
-            'FEEDBACK LENGTH',
-            arcs.length,
-        );
-        expect(arcs.length).toBeGreaterThan(0);
-    });
+    // it('detect cyclic graph', () => {
+    //     const initialArcs = graph.adj.flat();
+    //     const arcSet = service.feedbackArkSet(graph);
+    //     const arcs = arcSet.adj.flat();
+    //     console.log(
+    //         'ARC LENGTH',
+    //         initialArcs.length,
+    //         'FEEDBACK LENGTH',
+    //         arcs.length,
+    //     );
+    //     expect(arcs.length).toBeGreaterThan(0);
+    // });
 
-    xit('sort cyclic graph', () => {
-        const sort = service.topologicalSort(graph);
-        expect(sort.length).toBe(size);
-    });
+    // it('sort cyclic graph', () => {
+    //     const sort = service.topologicalSort(graph);
+    //     expect(sort.length).toBe(size);
+    // });
 });
--- queuest-zig/src/services/graph.zig
+++ queuest-zig/src/services/graph.zig
@@ -1,18 +1,19 @@
 const std = @import("std");
+const ArrayLU = std.ArrayListUnmanaged(gsize);
 
 const gsize = usize;
 
 pub const Graph = struct {
     size: gsize,
-    edges_size: usize,
-    edges: []std.ArrayListUnmanaged(gsize),
+    edges_size: gsize,
+    edges: []ArrayLU,
     a: std.mem.Allocator,
 
     pub fn init(a: std.mem.Allocator, size: gsize) Graph {
         const edge_size_init = if (size > 0) std.math.log2(size) + 2 else 0;
-        const e = a.alloc(std.ArrayListUnmanaged(gsize), size) catch unreachable;
+        const e = a.alloc(ArrayLU, size) catch unreachable;
         for (0..size) |i| {
-            e[i] = std.ArrayListUnmanaged(gsize).initCapacity(a, edge_size_init) catch unreachable;
+            e[i] = ArrayLU.initCapacity(a, edge_size_init) catch unreachable;
         }
         return Graph{
             .size = size,
@@ -34,22 +35,37 @@ pub const Graph = struct {
         self.edges[f].append(self.a, t) catch unreachable;
     }
 
-    pub fn isCyclic(self: *const Graph) !bool {
+    pub fn isCyclic(self: *const Graph) !?ArrayLU {
         const a = self.a;
         const visited: []bool = try a.alloc(bool, self.size);
         @memset(visited, false);
-        var stack = try std.ArrayListUnmanaged(gsize).initCapacity(a, self.size + self.edges_size);
+        var stack = try ArrayLU.initCapacity(a, self.size + self.edges_size);
         const in_stack: []bool = try a.alloc(bool, self.size);
         @memset(in_stack, false);
-        for (0..self.size) |i| {
+        const from: []gsize = try a.alloc(gsize, self.size);
+        var i: gsize = 0;
+        while (i < self.size) : (i += 1) {
+            // for (0..self.size) |i| {
             if (!visited[i]) try stack.append(a, i);
             while (stack.getLastOrNull()) |v| {
                 in_stack[v] = true;
                 var added = false;
                 for (self.edges[v].items) |w| {
-                    if (in_stack[w]) return true;
+                    if (in_stack[w]) {
+                        var result = try ArrayLU.initCapacity(a, stack.items.len);
+                        try result.append(a, w);
+                        from[w] = v;
+                        while (result.getLastOrNull()) |next| {
+                            try result.append(a, from[next]);
+                            if (from[next] == w) {
+                                break;
+                            }
+                        }
+                        return result;
+                    }
                     if (!visited[w]) {
                         try stack.append(a, w);
+                        from[w] = v;
                         added = true;
                     }
                 }
@@ -60,7 +76,7 @@ pub const Graph = struct {
                 }
             }
         }
-        return false;
+        return null;
     }
 };
 
@@ -97,10 +113,12 @@ test "is cyclic" {
     g.addEdge(6, 8);
 
     try expect(g.edges.len == 9);
-    try expect(try g.isCyclic() == false);
+    try expect(try g.isCyclic() == null);
 
     g.addEdge(4, 2);
-    try expect(try g.isCyclic() == true);
+    const cycle = try g.isCyclic();
+    try expect(cycle != null);
+    std.debug.print("Cycle {any}", .{cycle.?.items});
 }
 
 test "cyclic 500" {
@@ -113,9 +131,11 @@ test "cyclic 500" {
     const mil = std.time.microTimestamp();
     var g: Graph = Graph.init(a, size);
 
-    for (0..size) |i| {
-        for (0..size) |j| {
-            if ((i * 3 + j) % 11 == 0) {
+    var i: gsize = 0;
+    while (i < size) : (i += 1) {
+        var j: gsize = 0;
+        while (j < size) : (j += 1) {
+            if (cyrb53a(i, j) % 11 == 0) {
                 g.addEdge(i, j);
             }
         }
@@ -123,9 +143,75 @@ test "cyclic 500" {
     std.debug.print("\nTime to init: {d}\n", .{std.time.microTimestamp() - mil});
 
     const mili = std.time.microTimestamp();
-    for (0..1000) |i| {
-        _ = i;
-        try expect(try g.isCyclic() == true);
+    for (0..1000) |ii| {
+        _ = ii;
+        try expect(try g.isCyclic() != null);
     }
     std.debug.print("\nTime cyclic: {d}\n", .{std.time.microTimestamp() - mili});
+}
+
+const MAX_SIZE = 1_000_000_00;
+// var create = Date.now();
+// for (var del = 2; del < n; del++) {
+//     const graph = createGraph(n, del);
+//     var date = Date.now();
+//     for (var i = 0; i < Math.max(1000, times); i++) {
+//         expect(service.isGraphCylic(graph).length >= 0).toBeTruthy();
+//     }
+//     t += Date.now() - date;
+// }
+// console.log(`Time for ${n} size created ${Date.now() - create - t} times ${Math.max(1000, times)} in ms: ${t}`);
+
+test "cyclic test" {
+    const ca = std.heap.c_allocator;
+    var arena = std.heap.ArenaAllocator.init(ca);
+    var n: gsize = 2;
+    while (n < 2000) {
+        const nn: usize = @intCast(n);
+        var times: usize = MAX_SIZE / (nn * nn * nn * nn);
+        times = if (times < 1000) 1000 else times;
+        var t: i64 = 0;
+        const create = std.time.microTimestamp();
+        var del: gsize = 1;
+        while (del < n) : (del += 1) {
+            // for (2..n) |del| {
+            var graph = createGraph(arena.allocator(), n, del);
+            defer _ = arena.reset(.retain_capacity);
+            const date = std.time.microTimestamp();
+            for (0..times) |i| {
+                _ = i;
+                const tt = try graph.isCyclic();
+                try expect(tt == null or tt.?.items.len >= 0);
+            }
+            t += std.time.microTimestamp() - date;
+        }
+        std.debug.print("\nTime for {d} size created {d} times {d} in ms: {d}", .{ n, std.time.microTimestamp() - create - t, times, t });
+        n *= 2;
+    }
+}
+
+fn createGraph(a: std.mem.Allocator, size: gsize, del: gsize) Graph {
+    var g: Graph = Graph.init(a, size);
+    var i: gsize = 0;
+    while (i < size) : (i += 1) {
+        var j: gsize = 0;
+        while (j < size) : (j += 1) {
+            if (cyrb53a(i, j) % del == 0) {
+                g.addEdge(i, j);
+            }
+        }
+    }
+    return g;
+}
+
+fn cyrb53a(ii: gsize, jj: gsize) usize {
+    const i: usize = @intCast(ii);
+    const j: usize = @intCast(jj);
+    var h1 = 0xdeadbeef ^ i * 2654435761;
+    var h2 = 0x41c6ce57 ^ j * 1597334677;
+    h1 ^= h1 ^ (h2 >> 15) * 0x735a2d97;
+    h2 ^= h2 ^ (h1 >> 15) * 0xcaf649a9;
+    h1 ^= h2 >> 16;
+    h2 ^= h1 >> 16;
+    return 2097152 * (h2 >> 0) + (h1 >> 11);
 }
